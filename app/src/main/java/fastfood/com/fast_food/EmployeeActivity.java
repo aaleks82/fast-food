56b7ecda0a9952d2430f9cb49c435e7efd2de4b3
Veronika2303 <veronika.juric.95@gmail.com>
2018-11-12 12:03:56 +0100

    Layouts

---
+++ app/src/main/java/fastfood/com/fast_food/EmployeeActivity.java
@@ -0,0 +1,13 @@
+package fastfood.com.fast_food;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee);
+    }
+}
